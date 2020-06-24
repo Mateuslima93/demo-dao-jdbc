a7dad07cfa1f6ed933cdbfedded3b75d850a6499
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-06-24 14:41:03 -0300

    Classe entidade Vendedor

--- src/demo/dao/jdbc/DemoDaoJdbc.java
+++ src/demo/dao/jdbc/DemoDaoJdbc.java
@@ -1,12 +1,16 @@
 package demo.dao.jdbc;
 
+import java.util.Date;
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class DemoDaoJdbc {
 
     public static void main(String[] args) {
         Departamento derp = new Departamento(1, "books");
         System.out.println(derp);
+        Vendedor vend = new Vendedor(21,"Bob","bob@gmail.com",new Date(),3000.00,derp);
+        System.out.println(vend);
     }
     
 }
