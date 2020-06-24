a88f320b28f75fb8a95aacae896c352fa81a81cf
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-06-24 14:32:18 -0300

    Classe entidade Departamento

--- src/demo/dao/jdbc/DemoDaoJdbc.java
+++ src/demo/dao/jdbc/DemoDaoJdbc.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package demo.dao.jdbc;
 
-/**
- *
- * @author MASTER
- */
+import modelo.entidades.Departamento;
+
 public class DemoDaoJdbc {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Departamento derp = new Departamento(1, "books");
+        System.out.println(derp);
     }
     
 }
