a8a0c7429f98de7d93377712f39f3513a7766709
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-06-26 22:45:21 -0300

    Implementacao FindByDepartment

--- src/demo/dao/jdbc/DemoDaoJdbc.java
+++ src/demo/dao/jdbc/DemoDaoJdbc.java
@@ -1,7 +1,9 @@
 package demo.dao.jdbc;
 
+import java.util.List;
 import modelo.dao.DaoFactory;
 import modelo.dao.VendedorDao;
+import modelo.entidades.Departamento;
 import modelo.entidades.Vendedor;
 
 
@@ -12,6 +14,11 @@ public class DemoDaoJdbc {
         System.out.println("=== Test 1: seller findById");
         Vendedor vendedor = vendedorDao.findById(3);
         System.out.println(vendedor);
+        System.out.println("=== Test 2: seller findByDepartment");
+        List<Vendedor> list = vendedorDao.findByDepartament(new Departamento(2,null));
+        for (Vendedor obj: list) {
+            System.out.println(obj);
+        }
     }
     
 }
--- src/modelo/dao/impl/VendedorDaoJdbc.java
+++ src/modelo/dao/impl/VendedorDaoJdbc.java
@@ -7,7 +7,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import modelo.dao.VendedorDao;
 import modelo.entidades.Departamento;
 import modelo.entidades.Vendedor;
@@ -75,5 +78,38 @@ public class VendedorDaoJdbc implements VendedorDao {
         Vendedor vendedor = new Vendedor (rs.getInt("Id"),rs.getString("Name"),rs.getString("Email"),rs.getDate("BirthDate"),rs.getDouble("BaseSalary"),dep);
         return vendedor;
     }
-    
+
+    @Override
+    public List<Vendedor> findByDepartament(Departamento departamento) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " 
+                    + "FROM seller INNER JOIN department " 
+                    + "ON seller.DepartmentId = department.Id " 
+                    + "WHERE DepartmentId = ? " 
+                    + "ORDER BY Name");
+            st.setInt(1, departamento.getId());
+            rs = st.executeQuery();
+            List<Vendedor> listVend = new ArrayList<>();
+            Map<Integer,Departamento> map = new HashMap<>();
+            while (rs.next()){
+                Departamento dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                listVend.add(instantiateSeller(rs,dep));
+            }
+        return listVend;
+        }
+        catch(SQLException e){
+            throw new ConexaoIntegrityException(e.getMessage());
+        }
+        finally{
+            Conecao.closeResultSet(rs);
+            Conecao.closeStatement(st);
+        }
+    }
 }
